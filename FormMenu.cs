f305a618a66767f134d1f2a0e3057a7db00564dd
nisauliaftirni-ani <nisauliaftri@gmail.com>
2025-10-07 14:41:27 +0700

    frmmenu1

--- FormMenu.cs
+++ FormMenu.cs
@@ -16,5 +16,10 @@ namespace LatihanGithub
         {
             InitializeComponent();
         }
+
+        private void siswaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
